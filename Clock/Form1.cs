df049c5bd683691de8b45c582ce3b8570480b6b2
SomeshwarJ <senthilsomeshwar2002@gmail.com>
2023-10-26 23:03:10 +0530

    First commit

---
+++ Clock/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string date = DateTime.Now.ToString("dd-MM-yyyy");
+            label3.Text = date;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            label2.Text = time;
+        }
+    }
+}
